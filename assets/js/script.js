63bbbcad40be557335e04d6cf824e2a859c97c7a
Dorivania Santos <dorim.dev@gmail.com>
2024-04-07 18:15:51 -0300

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,10 @@ seletor: querySelector()
 let nome = window.document.getElementById("nome");
 let email = document.querySelector("#email");
 let assunto = document.querySelector("#assunto");
+let nomeOk = false;
+let emailOk = false;
+let assuntoOk = false;
+let mapa = document.querySelector("#mapa");
 
 nome.style.width = "30%";
 email.style.width = "30%";
@@ -22,12 +26,56 @@ assunto.style.width = "30%";
 
 //se o nome tiver menos de 3 letras
 function validaNome() {
-  let txt = document.querySelector("#txtNome");
+  let txtNome = document.querySelector("#txtNome");
   if (nome.value.length < 3) {
-    txt.innerHTML = "Nome inválido";
-    txt.style.color ='red'
+    txtNome.innerHTML = "Nome inválido";
+    txtNome.style.color = "red";
   } else {
-    txt.innerHTML = "Nome válido";
-    txt.style.color ='green'
+    txtNome.innerHTML = "Nome válido";
+    txtNome.style.color = "green";
+    nomeOk = true;
   }
+  //se dentro do valor do email não tiver @
+}
+function validaEmail() {
+  let txtEmail = document.querySelector("#txtEmail");
+  if (email.value.indexOf("@") == -1) {
+    txtEmail.innerHTML = "E-mail inválido";
+    txtEmail.style.color = "red";
+  } else {
+    txtEmail.innerHTML = "E-mail válido";
+    txtEmail.style.color = "green";
+    emailOk = true;
+  }
+}
+//assunto não passar de 100 caracteres
+function validaAssunto() {
+  let txtAssunto = document.querySelector("#txtAssunto");
+  if (assunto.value.length >= 100) {
+    txtAssunto.innerHTML =
+      "Texto muito grande, digite no máximo 100 caracteres.";
+    txtAssunto.style.color = "red";
+    txtAssunto.style.display = "block";
+  } else {
+    txtAssunto.style.display = "none";
+    assuntoOk = true;
+  }
+}
+
+function enviar() {
+  if (nomeOk == true && emailOk == true && assuntoOk == true) {
+    alert("Formulário enviado com sucesso!");
+  } else {
+    alert("Preencha o formulário corretamente antes de enviar!");
+  }
+}
+
+function mapaZoom() {
+  mapa.style.width = "800px";
+  mapa.style.heigth = "600px";
+}
+
+function mapaNormal() {
+  mapa.style.width = "400px";
+  mapa.style.heigth = "250px";
 }
